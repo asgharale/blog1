0fa9aeeabc0fbcabfa674bbbb0de4af9ff67a6e3
asgharale <asgharale11@gmail.com>
2024-01-12 15:24:16 -0800

    first intract with backend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,60 +1,15 @@
 <template>
 <div id="home" class="container-fluid m2-vr">
-  <div class="col-9 col-md-12 col-lg-11 wrapper">
-    <h1>Home </h1>
+  <div class="col-9 col-md-12 col-lg-11">
+    
+    <div id="latest_articles" class="container-fluid">
+      <article class="article wrapper p1 m2 col-3 col-md-5 col-sm-11" v-for="post in posts" :key="post.id">
+        <router-link to="/post">
+          <h1>{{ post.title }}</h1>
+        </router-link>
+      </article>
+    </div>
 
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
-    <h1>Some Text To make page scroll</h1>
   </div>
 
   <sidebar/>
@@ -62,17 +17,30 @@
 </template>
 
 <script>
+import axios from 'axios';
 import sidebar from '@/components/sidebar.vue';
 
 export default {
   name: 'HomeView',
+  data(){
+    return {
+      posts: []
+    }
+  },
   components: {
     sidebar,
+  },
+  mounted(){
+    axios.get("http://127.0.0.1:8000/siteapi/posts/v1/posts")
+    .then(response => this.posts = response.data.results)
   }
 }
 </script>
 <style scoped>
 #home{
   align-items: flex start;
+}
+.article{
+
 }
 </style>
